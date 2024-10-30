1d1d4d05c6721ef2ee9a866b3ebc54bab06f3a70
Alexandr_Legchilov <alexandr_legchilov@epam.com>
2024-10-30 11:51:24 +0300

    Add v2 implementation

--- test-e2e/step-definitions/index.ts
+++ test-e2e/step-definitions/index.ts
@@ -1,13 +1,14 @@
 import { Then } from '@cucumber/cucumber';
 import { expect } from 'chai';
+import { MemoryValue } from "@qavajs/core";
 
-Then('I expect {string} memory value to be equal {string}', async function (actual, expected) {
+Then('I expect {value} memory value to be equal {value}', async function (actual: MemoryValue, expected: MemoryValue) {
   const actualValue = await actual.value();
   const expectedValue = await expected.value();
   expect(expectedValue).to.eql(actualValue);
 });
 
-Then('I expect {string} memory value to contain {string}', async function (actual, expected) {
+Then('I expect {value} memory value to contain {value}', async function (actual: MemoryValue, expected: MemoryValue) {
   const actualValue = await actual.value();
   const expectedValue = await expected.value();
   expect(actualValue).to.contain(expectedValue);
